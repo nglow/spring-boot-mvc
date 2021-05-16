2862122b80c0c71d80c4f0df0c31fc89ebee0900
ndoom90 <ndoom90@gmail.com>
2021-05-16 20:21:39 +0900

    Update Contents
    1. HttpMessageConverters
    2. ViewResolve

---
+++ src/main/java/me/nglow/mvc/user/User.java
@@ -0,0 +1,23 @@
+package me.nglow.mvc.user;
+
+public class User {
+
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
--- src/main/java/me/nglow/mvc/user/UserController.java
+++ src/main/java/me/nglow/mvc/user/UserController.java
@@ -1,7 +1,6 @@
 package me.nglow.mvc.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -9,5 +8,11 @@ public class UserController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/users/create")
+    // @RestController를 사용하면 return type 앞에 @ResponseBody를 생략할 수 있음
+    public User create(@RequestBody User user) {
+         return user;
     }
 }
