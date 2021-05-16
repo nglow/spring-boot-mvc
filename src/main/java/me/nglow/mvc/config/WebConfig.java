d7a26f5a62da0aaa378128aa7edf594a10535ef2
ndoom90 <ndoom90@gmail.com>
2021-05-16 20:36:18 +0900

    Static Resource

--- src/main/java/me/nglow/mvc/config/WebConfig.java
+++ src/main/java/me/nglow/mvc/config/WebConfig.java
@@ -2,13 +2,19 @@ package me.nglow.mvc.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 // 추가적인 Web MVC 설정
 @Configuration
 //@EnableWebMvc // 이 Annotation을 사용하면 SpringBoot가 제공하는 Web MVC기능이 사라짐
 public class WebConfig implements WebMvcConfigurer {
-
-    // ...
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/m/**")
+                .addResourceLocations("classpath:/m/")
+                .setCachePeriod(20);
+    }
+// ...
 
 }
